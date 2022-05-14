89860cc1f855b07641f34947e048ba630ab324ed
lea tinoso <l.tinoso@damstratechnology.com>
2022-05-14 16:14:40 +0800

    Installs React router V6 and adds routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* navbar component goes here */}
+        <Routes>
+          <Route exact path="/quotes">
+            {/* landing page component goes here */}
+          </Route>
+          <Route>
+            {/* quote component goes here
+            include uuid in path
+            ex: /quotes/:uuid */}
+          </Route>
+          <Route>
+            {/* add quote component goes here */}
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
